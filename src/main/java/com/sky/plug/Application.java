a29597fa816d8c881268a7b4cd5e192cfaee1360
shizhanwei <shizhanwei123@126.com>
2020-02-14 14:22:57 +0800

    mybatis插件开发demo

---
+++ src/main/java/com/sky/plug/Application.java
@@ -0,0 +1,10 @@
+package com.sky.plug;
+
+/**
+ * Hello world!
+ */
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/com/sky/plug/ApplicationTest.java
@@ -0,0 +1,18 @@
+package com.sky.plug;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class ApplicationTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
